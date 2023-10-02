73a14085381ba40c844f0e61ab9defb04da76789
alivkaaa <a.ivanevuch@gmail.com>
2023-10-02 14:32:43 +0300

    1

---
+++ Untitled-1.java
@@ -0,0 +1,4 @@
+http://backend.course#alivkaaa
+http://backend.course?name=”Іваневич Аліна”&github_name=alivkaaa&group=”ФЕІ-21”&subgroup=”Підгрупа 1”
+http%3A%2F%2Fbackend%2Ecourse%3Fname%3D%E2%80%9D%D0%86%D0%B2%D0%B0%D0%BD%D0%B5%D0%B2%D0%B8%D1%87%20%D0%90%D0%BB%D1%96%D0%BD%D0%B0%E2%80%9D%26github%5Fname%3Dalivkaaa%26group%3D%E2%80%9D%D0%A4%D0%95%D0%86%2D21%E2%80%9D%26subgroup%3D%E2%80%9D%D0%9F%D1%96%D0%B4%D0%B3%D1%80%D1%83%D0%BF%D0%B0%201%E2%80%9D
+https://bank.gov.ua/NBUStatService/v1/statdirectory/inflation?period=y&date=202201&json
